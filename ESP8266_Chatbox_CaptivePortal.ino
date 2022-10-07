3320e65cdac8f3bee26a7dea0132f4d19e26780a
iotool <roha.mbox@gmail.com>
2022-10-08 01:18:00 +0200

    V-20221008-0106
    Bugfix dynamic html, substring input
    Show input counter

--- ESP8266_Chatbox_CaptivePortal.ino
+++ ESP8266_Chatbox_CaptivePortal.ino
@@ -15,6 +15,8 @@
 // OTA update for ArduinoDroid
 // OTA password protect by cli
 // admin cli for status, ota, restart
+// dynamic html without memory issue
+// validate input size
 
 // overwrite esp-sdk (wifi_set_country)
 extern "C" {
@@ -28,6 +30,8 @@ extern "C" {
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+/* --- configuration --- */
+
 #define AP_PHYMOD  WIFI_PHY_MODE_11B
 #define AP_POWER   20    // 0..20 Lo/Hi
 #define AP_CHANNEL 13    // 1..13
@@ -38,10 +42,14 @@ extern "C" {
 #define ESP_RTCMEM 443   // buffer rtc
 #define LED_OFF    0
 #define LED_ON     1
+#define CHAT_MLEN  56    // msg length
+#define CHAT_MARY  64    // msg array
 
 const char* apName = "Chatbox-";
 const char* apPass = "";
 const char* apHost = "web";
+
+/* --- variables --- */
 
 DNSServer dnsServer;
 ESP8266WebServer httpServer(80);
@@ -50,7 +58,6 @@ ESP8266WebServer httpServer(80);
 String apSSID="";
 String urlHome="";
 String urlChatRefresh="4,url=";
-String chatData="Admin: Chat gestartet";
 
 // use rtc memory as backup
 typedef struct { 
@@ -59,14 +66,20 @@ typedef struct {
 } rtcStore;
 rtcStore rtcMem;
 
-// Flags
-struct {
-  uint8_t enableCliCommand:1;
-  uint8_t enableOtaUpdate:1;
-  uint8_t disconnectClients:1;
-  uint8_t builtinLedMode:2;
-  uint8_t reserve:3;
-} flag = {0,0,0,0,0};
+// V1: use DRAM for buffer
+String chatData="Admin: Chat gestartet";
+
+// V2: 64B/message (static ~ nicht DRAM)
+typedef struct {
+  uint16_t node;     // mac address node
+  uint16_t id:10;    // sequence message
+  uint8_t  mlen:6;   // length message
+  uint8_t  slen:4;   // length sender
+  uint8_t  rlen:4;   // length receiver
+  uint32_t age:24;   // 250ms slots
+  char     data[CHAT_MLEN]; // buffer
+} chatMsgT;
+static chatMsgT chatMsgB[CHAT_MARY]={0};
 
 void setup() {
 
@@ -173,16 +186,26 @@ void setup() {
   delay(100);
 }
 
+/* --- main --- */
+
+// Flags
+struct {
+  uint8_t enableCliCommand:1;
+  uint8_t enableOtaUpdate:1;
+  uint8_t disconnectClients:1;
+  uint8_t builtinLedMode:2;
+  uint8_t reserve:3;
+} flag = {0,0,0,0,0};
+
+// time
 uint32_t uptime=0;
 uint32_t uptimeLast=0;
-uint8_t requests=0;
-uint8_t disconnectTimer=0;
 
 void loop() {
-  uint32_t uptimeLast=uptime;
+  uptimeLast=uptime;
   uptime=millis();
   if (uptime!=uptimeLast) {
-    // every millisecond
+    // only one per millisecond
     doLed();
     doDebug();
     doServer();
@@ -191,6 +214,27 @@ void loop() {
   }
   yield();
 }
+
+void doServer() {
+  if (uptime%11==0) {
+    // 9: 111 Req/s ~  5.4 KB/Req
+    // 11: 90 Req/s ~  6.6 KB/Req
+    // 13: 76 Req/s ~  7.7 KB/Req
+    // 17: 58 Req/s ~ 10.3 KB/Req
+    dnsServer.processNextRequest();
+    yield();
+    httpServer.handleClient();
+    yield();
+    if (flag.enableOtaUpdate == 1) {
+      ArduinoOTA.handle();
+      yield();
+    }
+  }
+}
+
+/* --- resilience --- */
+
+uint8_t disconnectTimer=0;
 
 void doDisconnect() {
   // disconnect all clients
@@ -231,6 +275,87 @@ void doDisconnect() {
   }
 }
 
+void doReboot() {
+  // reboot before out of memory freeze
+  if (uptime%103==0) {
+    if (system_get_free_heap_size()
+        < ESP_OUTMEM) {
+      doBackup();
+      delay(300);
+      WiFi.softAPdisconnect(true);
+      yield();
+      delay(200);
+      ESP.restart();
+    }
+  }
+}
+
+/* --- debugging --- */
+
+void doLed() {
+  switch(flag.builtinLedMode) {
+    case LED_OFF:
+      digitalWrite(LED_BUILTIN,HIGH);
+      break;
+    case LED_ON:
+      digitalWrite(LED_BUILTIN,LOW);
+      break;
+  }
+  yield();
+}
+
+uint8_t requests=0;
+
+void doDebug(){
+  if (uptime%1009==0) {
+    // debug status
+    Serial.print(uptime);
+    Serial.print(F("ms,"));
+    Serial.print(
+      ESP.getFreeHeap());
+    Serial.print(F("B,"));
+    Serial.print(requests);
+    Serial.print(F("R,"));
+    Serial.print(
+      WiFi.softAPgetStationNum());    
+    Serial.print(F("C,"));
+    Serial.print(disconnectTimer);    
+    Serial.print(F("D,"));
+    Serial.print(rtcMem.check);    
+    Serial.print(F("T"));
+    Serial.println();
+    requests=0;
+    yield();
+  }
+}
+
+/* --- chat data --- */
+
+String getChat() {
+  return chatData;
+}
+    
+void addChat(
+  String ms, 
+  String mr,
+  String mb) {
+  if (mr.length()>0) {
+    ms+="@"+mr;
+  }
+  if (ms.length()>0) {
+    ms=ms+": ";
+  }
+  if (ms.length()+mb.length()>0) {
+    chatData=ms+mb+"<br><br>"+chatData;
+    chatData=
+      chatData.substring(0,AP_CHATMEM);
+    doBackup();
+  }
+  ms="";
+  mr="";
+  mb="";
+}
+
 void doBackup() {
   rtcMem.check = 0xDE49;
   if (chatData.length()<ESP_RTCMEM){
@@ -249,83 +374,10 @@ void doBackup() {
   yield();
 }
 
-void doReboot() {
-  if (uptime%107==0) {
-    if (system_get_free_heap_size()
-        < ESP_OUTMEM) {
-      doBackup();
-      delay(300);
-      WiFi.softAPdisconnect(true);
-      yield();
-      delay(200);
-      ESP.restart();
-    }
-  }
-}
-
-void doLed() {
-  switch(flag.builtinLedMode) {
-    case LED_OFF:
-      digitalWrite(LED_BUILTIN,HIGH);
-      break;
-    case LED_ON:
-      digitalWrite(LED_BUILTIN,LOW);
-      break;
-  }
-  yield();
-}
-
-void doDebug(){
-  if (uptime%1009==0) {
-    // debug status
-    Serial.print(uptime);
-    Serial.print(F("ms,"));
-    Serial.print(
-      system_get_free_heap_size());
-    Serial.print(F("B,"));
-    Serial.print(requests);
-    Serial.print(F("R,"));
-    Serial.print(
-      WiFi.softAPgetStationNum());    
-    Serial.print(F("C,"));
-    Serial.print(disconnectTimer);    
-    Serial.print(F("D,"));
-    Serial.print(rtcMem.check);    
-    Serial.print(F("T"));
-    Serial.println();
-    requests=0;
-    yield();
-  }
-}
-
-void doServer() {
-  if (uptime%9==0) {
-    // 9: 111 Req/s ~  5.4 KB/Req
-    // 11: 90 Req/s ~  6.6 KB/Req
-    // 13: 76 Req/s ~  7.7 KB/Req
-    // 17: 58 Req/s ~ 10.3 KB/Req
-    dnsServer.processNextRequest();
-    yield();
-    httpServer.handleClient();
-    yield();
-    if (flag.enableOtaUpdate == 1) {
-      ArduinoOTA.handle();
-      yield();
-    }
-  }
-}
-
-String getChat() {
-  return chatData;
-}
-    
-void addChat(String usr, String msg) {
-  chatData = usr+": "+msg+"<br><br>"+chatData;
-  chatData.substring(0,AP_CHATMEM);
-  doBackup();
-}
+/* --- http handler --- */
 
 void onHttpToHome() {
+  // redirect to home
   httpServer.sendHeader(
     F("Location"), urlHome
   );
@@ -337,6 +389,7 @@ void onHttpToHome() {
 }
 
 void onHttpHome() {
+  // tiny static landing page
   httpServer.send(
     200, F("text/html"), F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><br>Dieser Hotspot bietet einen lokalen Chat. Die Nachrichten werden nicht dauerhaft gespeichert. Bitte alle rechtlichen Regeln einhalten und keine Beleidigungen!<br><br><a href=/chat>OK - akzeptiert</a></body></html>")
   );
@@ -345,10 +398,16 @@ void onHttpHome() {
 }
 
 void onHttpChat() {
+  // chat messages with autorefresh
   httpServer.sendHeader(
     F("Refresh"), F("8")
   );
-  /* --- bug garbage collection ---
+  /* -- bugfix garbage collection -- */
+  uint32_t timestart = millis();
+  uint16_t timeout = 500;
+  httpServer.sendHeader(
+    F("Connection"), F("close")
+  );
   httpServer.setContentLength(
     CONTENT_LENGTH_UNKNOWN
   );
@@ -356,13 +415,24 @@ void onHttpChat() {
     200,F("text/html"),F("")
   );
   httpServer.sendContent(
-    F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body>chat <a href=/chatf>add</a><hr>"));
+    F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><a href=/chatf>neue Nachricht</a><hr><br>"));
+  httpServer.sendContent(getChat());
   httpServer.sendContent(
     F("</body></html>"));
+  yield();
+  // close connection
   httpServer.sendContent(F(""));
+  while (
+    httpServer.client().available()) {
+    httpServer.client().read();
+    yield();
+    if (millis()-timestart>timeout) {
+      break;
+    }
+  }
+  yield();
   httpServer.client().stop();
-  --- */
-  /* dynamic html */
+  /* -- dynamic html with string -- *
   String html="";
   html+=F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><a href=/chatf>neue Nachricht</a><hr><br>");
   html+=getChat();
@@ -371,13 +441,14 @@ void onHttpChat() {
     200, F("text/html"),html
   );
   html="";
+  * -- */
   requests++;
   yield();
 }
 
 void onHttpChatFrm() {
   httpServer.send(
-    200, F("text/html"), F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><a href=/chat>abbrechen</a><hr><br><form action=/chata method=POST>Absender:<br><input type=text name=usr><br><br>Nachricht:<br><textarea name=msg rows=3 cols=22></textarea><br><br><input type=submit value=senden></form></body></html>")
+    200, F("text/html"), F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /><script>function onInp(){var df=document.forms.mf,cl=56-(df.ms.value.length+df.mr.value.length+df.mb.value.length); document.getElementById('mn').innerText=cl;}</script></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><a href=/chat>abbrechen</a><hr><br><form name=mf action=/chata method=POST>Absender:<br><input type=text name=ms maxlength=16 onChange=onInp() onkeyup=onInp()><br><br>Empf&auml;nger:<br><input type=text name=mr maxlength=16 onChange=onInp() onkeyup=onInp()><br><br>Nachricht:<br><textarea name=mb rows=3 cols=22 maxlength=56 onChange=onInp() onkeyup=onInp()></textarea><br><br><input type=submit value=senden> <span id=mn></span></form></body></html>")
   );
   requests++;
   yield();
@@ -397,28 +468,35 @@ void onHttpChatAdd() {
   httpServer.sendHeader(
     F("Refresh"), urlChatRefresh
   );
-  String usr = maskHttpArg("usr");
-  usr.substring(0,20);
-  String msg = maskHttpArg("msg");
-  msg.substring(0,64-usr.length());
-  addChat(usr,msg);
+  String ms = maskHttpArg("ms");
+  ms=ms.substring(0,16);
+  String mr = maskHttpArg("mr");
+  mr=mr.substring(0,16);
+  String mb = maskHttpArg("mb");
+  mb=mb.substring(
+    0,56-ms.length()-mr.length());
+  addChat(ms,mr,mb);
   String html="";
   html+=F("<html><head><meta name='viewport' content='width=device-width, initial-scale=1' /></head><body bgcolor=#003366 text=#FFFFCC link=#66FFFF vlink=#66FFFF alink=#FFFFFF><h1>WiFi Chat</h1><hr><a href=/chat>weiter</a><hr><br>");
-  html+=usr;
+  html+=ms;
+  if (mr.length()>0) {
+    html+=F("@");
+    html+=mr;
+  }
   html+=F(": ");
-  html+=msg;
+  html+=mb;
   html+=F("</body></html>");
   httpServer.send(
     200, F("text/html"),html
   );
-  html=""; usr=""; msg="";
+  html=""; ms=""; mr=""; mb="";
   requests++;
   yield();
 }
 
 void onHttpCli() {
   String text= F(
-    "Version: 20221007-1146\n"
+    "Version: 20221008-0106\n"
     "/cli?cmd=login-password\n"
     "/cli?cmd=logoff\n"
     "/cli?cmd=restart\n"
@@ -475,6 +553,7 @@ void onHttpCli() {
       flag.builtinLedMode=LED_ON;
     }
   }
+  cmd="";
   requests++;
   yield();
 }
